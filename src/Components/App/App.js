398bac2b02ff231918c766865f6fb943ccda7460
Alexander <mishinall1991@gmail.com>
2022-06-10 18:55:04 +0300

    Hooks rules

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,12 +3,20 @@ import React, {useState} from "react";
 const App = () => {
     const [name, setName] = useState("Joing");
     const [age, setAge] = useState(25);
-    const [rate, setRate] useState(25);
+    const [rate, setRate] = useState(25);
 
     return (
         <div style={{width: "550px", margin: "0 auto"}}>
-            {name} is {age} year old
-            his rate is ${rate} per hout
+
+            <h2 style={{textAlign: "center"}}>useEffect</h2>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <img style={{height: "190px", marginRight: "10px"}} src={UseCollback_useMemo_img} alt=""/>
+                <div>
+                    {name} is {age} year old
+                    his rate is ${rate} per hout
+                </div>
+            </div>
+
         </div>
     );
 }
